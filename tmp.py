0ab403fb468e491b1bca7ca0e959302c293da693
JarvisSu <jarvis_su@126.com>
2020-07-22 21:17:18 +0800

    add ignore file

--- tmp.py
+++ tmp.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 print("hello ")
 
-file = 'D:\OneDrive\workSpace\Careate\互联网医院\医生医院数据\复诊名单.xlsx'
+file = 'C:/Users/JarvisSu/OneDrive/workSpace/Careate/互联网医院/医生医院数据/复诊名单.xlsx'
 
 data = pd.read_excel(file, sheet_name = 'base_doctor')
 print(data.head())
